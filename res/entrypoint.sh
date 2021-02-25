ea20e1eacf49cbb277998978c0380e797f167d59
Hayden <hansson.hayden@me.com>
2021-02-25 20:10:41 +0800

    beta branch fix

--- res/entrypoint.sh
+++ res/entrypoint.sh
@@ -4,7 +4,7 @@ cd /home/container
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
-if [ "${BRANCH}" = 1 ];
+if [ ! -z "${BRANCH}" ];
 then
     echo "Updating ${BRANCH} Branch"
     # Update Rust Server
